d5e6d485232818aa7edeba55e810ee80e79d2f30
mbellucio <=>
2024-03-30 00:08:04 -0300

    fix nan population bug

--- src/js/script.js
+++ src/js/script.js
@@ -31,7 +31,9 @@ const planetDetails = (planet) => {
         <p class="fs-5 m-1">Climate: ${planet.climate}</p>
       </div>
       <div>
-        <p class="m-1">${formatter.format(planet.population)} habitants</p>
+        <p class="m-1">${
+          isNaN(planet.population) ? "no" : formatter.format(planet.population)
+        } habitants</p>
       </div>
       <div>
         <p class="m-1">Biome: ${planet.terrain}</p>
